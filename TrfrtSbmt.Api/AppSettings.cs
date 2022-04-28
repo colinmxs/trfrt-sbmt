42059949b1c5b1113c0f6ec534fbc504d3907d27
Colin Smith <colinsmith1@boisestate.edu>
2022-04-27 21:36:56 -0600

    setup api components

---
+++ TrfrtSbmt.Api/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace TrfrtSbmt.Api;
+using System.Collections.Generic;
+
+public class AppSettings
+{
+    public AppSettings(ConfigurationManager config)
+    {
+        config.GetSection("AppSettings").Bind(this);
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        ArgumentNullException.ThrowIfNull(environment, "ASPNETCORE_ENVIRONMENT");
+        EnvironmentName = environment;
+    }
+    public string EnvironmentName { get; set; }
+}
--- TrfrtSbmt.Api/Program.cs
+++ TrfrtSbmt.Api/Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddCors();
+// add typed appsettings file
+ConfigurationManager configuration = builder.Configuration;
+var appSettings = new AppSettings(configuration);
+builder.Services.AddSingleton(appSettings);
 
+// Add other services to the container.
+builder.Services.AddMediatR(typeof(Program));
+
+// Add AWS Lambda support.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+// configure auth
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+app.UseCors(builder => builder
+              .AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// endpoints
 app.MapGet("/", () => "Treefort Submit Api!");
 
 app.Run();
