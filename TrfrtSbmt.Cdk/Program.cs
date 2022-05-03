12c5d21e1def8d52d4be368a7979d7c26633c562
Colin Smith <colinsmith1@boisestate.edu>
2022-05-02 21:50:40 -0600

    fix build

--- TrfrtSbmt.Cdk/Program.cs
+++ TrfrtSbmt.Cdk/Program.cs
@@ -10,7 +10,10 @@ Tags.Of(app).Add("Owner", "smith.colin00@gmail.com");
 Tags.Of(app).Add("Application", "Submit Api");
 
 var accountId = (string)app.Node.TryGetContext("accountid");
-_ = new DbStack(app, "DbStack", new StackProps(), regions.Except(new List<string> { "use-west-2" }).ToArray());
+_ = new DbStack(app, "DbStack", new StackProps
+{
+    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+}, regions.Except(new List<string> { "use-west-2" }).ToArray());
 foreach (var region in regions)
 {
     var otherRegions = regions.Except(new List<string> { region });
