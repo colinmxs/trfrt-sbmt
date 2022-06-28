ac188fd483c0bafe0536e9cb1fbbc6237dcdd2b6
Colin Smith <colinsmith1@boisestate.edu>
2022-06-28 09:33:54 -0600

    stub new test method

---
+++ TrfrtSbmt.Tests/Submissions/GetUploadUrlTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrfrtSbmt.Api.Features.Festivals;
+using System;
+using static TrfrtSbmt.Tests.TestFixture;
+using System.Threading.Tasks;
+using Shouldly;
+using System.Linq;
+using System.Collections.Generic;
+using static TrfrtSbmt.Api.Features.Submissions.ListSubmissions;
+using TrfrtSbmt.Api.Features.Submissions;
+using TrfrtSbmt.Api.Features.Forts;
+using CodenameGenerator;
+using TrfrtSbmt.Api.DataModels;
+
+namespace TrfrtSbmt.Tests.Submissions;
+
+[TestClass]
+public class GetUploadUrlTests
+{
+    [TestMethod]
+    public async Task SmokeTest()
+    {
+
+    }
+}
