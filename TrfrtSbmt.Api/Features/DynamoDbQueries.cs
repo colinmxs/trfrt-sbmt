4d69c548e55cf8d52ba45c02e499cf71131fc4be
Colin Smith <colinsmith1@boisestate.edu>
2022-07-17 20:54:51 -0600

    fix index name

--- TrfrtSbmt.Api/Features/DynamoDbQueries.cs
+++ TrfrtSbmt.Api/Features/DynamoDbQueries.cs
@@ -132,7 +132,7 @@ public class DynamoDbQueries
                     {
                         {":pk", new AttributeValue(createdBy)}
                     },
-                IndexName = "CreatedByQuery"
+                IndexName = "CreatedByIndex"
             });
         }
     }
