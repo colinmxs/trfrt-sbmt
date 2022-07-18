01215b9d86edf237a57ab69fe69bcd043c124e9d
Colin Smith <colinsmith1@boisestate.edu>
2022-07-17 21:33:29 -0600

    add labels to vm

--- TrfrtSbmt.Api/Features/Submissions/SubmissionViewModel.cs
+++ TrfrtSbmt.Api/Features/Submissions/SubmissionViewModel.cs
@@ -17,10 +17,43 @@ public record SubmissionViewModel(string FestivalId,
                                       string Statement,
                                       IEnumerable<string> Genres,
                                       SocialLinksVm Links,
-                                      ContactInfoVm ContactInfo)
+                                      ContactInfoVm ContactInfo,
+                                      IEnumerable<LabelVm> Labels)
 {
-    public SubmissionViewModel(Submission submission) : this(submission.FestivalId, submission.PartitionKey, submission.EntityId, submission.SubmissionDate, submission.Name, submission.State, submission.City, submission.Country, submission.Description, submission.Image, submission.Website, submission.Statement, submission.Genres, JsonSerializer.Deserialize<SocialLinksVm>(submission.Links), JsonSerializer.Deserialize<ContactInfoVm>(submission.ContactInfo)) { }
+    public SubmissionViewModel(Submission submission) : this(
+        submission.FestivalId,
+        submission.PartitionKey,
+        submission.EntityId,
+        submission.SubmissionDate,
+        submission.Name,
+        submission.State,
+        submission.City,
+        submission.Country,
+        submission.Description,
+        submission.Image,
+        submission.Website,
+        submission.Statement,
+        submission.Genres,
+        JsonSerializer.Deserialize<SocialLinksVm>(submission.Links),
+        JsonSerializer.Deserialize<ContactInfoVm>(submission.ContactInfo),
+        submission.Labels.Select(l => new LabelVm(l.Id, l.Name))) { }
 }
 
-public record SocialLinksVm(string? Spotify, string? AppleMusic, string? Bandcamp, string? Soundcloud, string[]? Videos, string? Facebook, string? Twitter, string? Instagram, string? TikTok);
-public record ContactInfoVm(string? Name, string? PhoneNumber, string Email, string? Relationship, string? ManagementContact, string? AgentContact, string? PublicityContact, string? LabelContact);
+public record LabelVm(string Id, string Name);
+public record SocialLinksVm(string? Spotify,
+                            string? AppleMusic,
+                            string? Bandcamp,
+                            string? Soundcloud,
+                            string[]? Videos,
+                            string? Facebook,
+                            string? Twitter,
+                            string? Instagram,
+                            string? TikTok);
+public record ContactInfoVm(string? Name,
+                            string? PhoneNumber,
+                            string Email,
+                            string? Relationship,
+                            string? ManagementContact,
+                            string? AgentContact,
+                            string? PublicityContact,
+                            string? LabelContact);
