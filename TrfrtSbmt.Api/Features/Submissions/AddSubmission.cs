fb491fb239f16de825dc13acc81b571e8a3a9c3c
Colin Smith <colinsmith1@boisestate.edu>
2022-07-20 19:03:56 -0600

    update submission email message

--- TrfrtSbmt.Api/Features/Submissions/AddSubmission.cs
+++ TrfrtSbmt.Api/Features/Submissions/AddSubmission.cs
@@ -7,6 +7,7 @@ using Amazon.SimpleEmailV2.Model;
 using System.Security.Claims;
 using System.Text.Json;
 using TrfrtSbmt.Api.DataModels;
+using TrfrtSbmt.Api.Utils.DiscordWebhooks;
 
 public class AddSubmission
 {
@@ -23,12 +24,15 @@ public class AddSubmission
         private readonly IAmazonSimpleEmailServiceV2 _emailer;
         private readonly AppSettings _settings;
         private readonly ClaimsPrincipal _user;
-        public AddSubmissionCommandHandler(IAmazonDynamoDB db, IAmazonSimpleEmailServiceV2 emailer, AppSettings settings, ClaimsPrincipal user)
+        private readonly IDiscordWebhookClient _discord;
+
+        public AddSubmissionCommandHandler(IAmazonDynamoDB db, IAmazonSimpleEmailServiceV2 emailer, AppSettings settings, ClaimsPrincipal user, IDiscordWebhookClient discord)
         {
             _db = db;
             _emailer = emailer;
             _settings = settings;
             _user = user;
+            _discord = discord;
         }
 
         public async Task<SubmissionViewModel> Handle(AddSubmissionCommand request, CancellationToken cancellationToken)
@@ -109,7 +113,11 @@ public class AddSubmission
                                     Text = new Content
                                     {
                                         Charset = "UTF-8",
-                                        Data = @"Your submission to Treefort 11 has been received! We will notify all submissions of their status no later than January 15th, 2023. Submissions can be edited in the Treefort Submissions App."
+                                        Data = @"Thank you for submitting to perform at Treefort Music Fest 2023. Our Artist & Fort Committees are currently reviewing all submissions, please stay tuned for a response. All submitted artists will be notified no later than January 15th, 2023 regarding your status to play at Treefort Music Fest. Submissions can be edited within the Treefort Submissions App, if needed.
+
+Until then, please follow us on our socials and sign up to receive our emails: http://eepurl.com/FO5tT
+@treefortfest
+#treefort11"
                                     }
                                 }
                             }
