4842e6122e63139b4a88ebf8ad9878df8ad82671
Colin Smith <colinsmith1@boisestate.edu>
2022-07-31 13:13:17 -0600

    try only deploy backend

--- TrfrtSbmt.Cdk/Program.cs
+++ TrfrtSbmt.Cdk/Program.cs
@@ -18,29 +18,31 @@ var api = new ApiStack(app, $"{envPrefix}TrfrtSbmt-ApiStack-{region}", new ApiSt
     EnvironmentName = env,
     EnvironmentPrefix = envPrefix
 });
-
-var dbs = new DbStack(app, $"{envPrefix}TrfrtSbmt-DbStack", new DbStackProps
+if (false)
 {
-    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
-    EnvironmentName = env,
-    EnvironmentPrefix = envPrefix
-});
+    var dbs = new DbStack(app, $"{envPrefix}TrfrtSbmt-DbStack", new DbStackProps
+    {
+        Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+        EnvironmentName = env,
+        EnvironmentPrefix = envPrefix
+    });
 
-var s3 = new S3Stack(app, $"{envPrefix}TrfrtSbmt-S3Stack", new S3Stack.S3StackProps() 
-{    
-    EnvironmentName = env,
-    EnvironmentPrefix = envPrefix
-});
+    var s3 = new S3Stack(app, $"{envPrefix}TrfrtSbmt-S3Stack", new S3Stack.S3StackProps() 
+    {    
+        EnvironmentName = env,
+        EnvironmentPrefix = envPrefix
+    });
 
-var iam = new IamStack(app, $"{envPrefix}TrfrtSbmt-IamStack", new IamStack.IamStackProps
-{
-    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
-    Role = api.LambdaExecutionRole,
-    Table = dbs.Table,
-    TestTable = dbs.TestTable,
-    Bucket = s3.Bucket,
-    EnvironmentName = env,
-    EnvironmentPrefix = envPrefix
-});
+    var iam = new IamStack(app, $"{envPrefix}TrfrtSbmt-IamStack", new IamStack.IamStackProps
+    {
+        Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+        Role = api.LambdaExecutionRole,
+        Table = dbs.Table,
+        TestTable = dbs.TestTable,
+        Bucket = s3.Bucket,
+        EnvironmentName = env,
+        EnvironmentPrefix = envPrefix
+    });
+}
 
 app.Synth();
