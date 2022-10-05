18f2ee4fc3450f6ea7399acea03edd4591388e3a
Colin Smith <colinsmith1@boisestate.edu>
2022-10-04 18:45:46 -0600

    try fix stack

--- TrfrtSbmt.Cdk/Program.cs
+++ TrfrtSbmt.Cdk/Program.cs
@@ -20,40 +20,39 @@ var api = new ApiStack(app, $"{envPrefix}TrfrtSbmt-ApiStack-{region}", new ApiSt
     EnvironmentName = env,
     EnvironmentPrefix = envPrefix
 });
-if (true)
+
+var dbs = new DbStack(app, $"{envPrefix}TrfrtSbmt-DbStack", new DbStackProps
 {
-    var dbs = new DbStack(app, $"{envPrefix}TrfrtSbmt-DbStack", new DbStackProps
-    {
-        Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
-        EnvironmentName = env,
-        EnvironmentPrefix = envPrefix
-    });
+    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+    EnvironmentName = env,
+    EnvironmentPrefix = envPrefix
+});
 
-    var voteStreamStack = new VoteStreamStack(app, $"{envPrefix}TrfrtSbmt-VoteStreamStack", new VoteStreamStack.VoteStreamStackProps
-    {
-        Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
-        EnvironmentName = env,
-        EnvironmentPrefix = envPrefix,
-        Table = dbs.Table
-    });
+var voteStreamStack = new VoteStreamStack(app, $"{envPrefix}TrfrtSbmt-VoteStreamStack", new VoteStreamStack.VoteStreamStackProps
+{
+    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+    EnvironmentName = env,
+    EnvironmentPrefix = envPrefix,
+    Table = dbs.Table
+});
 
-    var s3 = new S3Stack(app, $"{envPrefix}TrfrtSbmt-S3Stack", new S3Stack.S3StackProps() 
-    {    
-        EnvironmentName = env,
-        EnvironmentPrefix = envPrefix
-    });
+var s3 = new S3Stack(app, $"{envPrefix}TrfrtSbmt-S3Stack", new S3Stack.S3StackProps()
+{
+    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+    EnvironmentName = env,
+    EnvironmentPrefix = envPrefix
+});
 
-    var iam = new IamStack(app, $"{envPrefix}TrfrtSbmt-IamStack", new IamStack.IamStackProps
-    {
-        Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
-        Role = api.LambdaExecutionRole,
-        Table = dbs.Table,
-        TestTable = dbs.TestTable,
-        Bucket = s3.Bucket,
-        EnvironmentName = env,
-        EnvironmentPrefix = envPrefix,
-        VoteRole = voteStreamStack.LambdaExecutionRole
-    });
-}
+var iam = new IamStack(app, $"{envPrefix}TrfrtSbmt-IamStack", new IamStack.IamStackProps
+{
+    Env = new Amazon.CDK.Environment { Region = "us-west-2", Account = accountId },
+    Role = api.LambdaExecutionRole,
+    Table = dbs.Table,
+    TestTable = dbs.TestTable,
+    Bucket = s3.Bucket,
+    EnvironmentName = env,
+    EnvironmentPrefix = envPrefix,
+    VoteRole = voteStreamStack.LambdaExecutionRole
+});
 
 app.Synth();
