31149a7a26a9db6da220cbc17ce73f13ddab1d4a
Colin Smith <colinsmith1@boisestate.edu>
2022-10-27 18:53:18 -0600

    add pagination to vote tally

--- TrfrtSbmt.Api/Features/DynamoDbQueries.cs
+++ TrfrtSbmt.Api/Features/DynamoDbQueries.cs
@@ -161,7 +161,7 @@ public class DynamoDbQueries
     {
         public VoteTallyQuery(IAmazonDynamoDB db, AppSettings settings) : base(db, settings) { }
 
-        public async Task<QueryResponse> ExecuteAsync(string fortId, Dictionary<string, AttributeValue> lastEvaluatedKey)
+        public async Task<QueryResponse> ExecuteAsync(string fortId, bool forward, int pageSize, Dictionary<string, AttributeValue>? lastEvaluatedKey)
         {
             return await _db.QueryAsync(new QueryRequest(_settings.TableName)
             {
@@ -171,6 +171,8 @@ public class DynamoDbQueries
                     {":pk", new AttributeValue(fortId)}
                 },
                 IndexName = "AverageScoreIndex",
+                ScanIndexForward = forward,
+                Limit = pageSize,
                 ExclusiveStartKey = lastEvaluatedKey
             });
         }
--- TrfrtSbmt.Api/Features/Voting/GetVoteTally.cs
+++ TrfrtSbmt.Api/Features/Voting/GetVoteTally.cs
@@ -1,13 +1,55 @@
 ﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System.Text;
 using TrfrtSbmt.Api.DataModels;
 
 namespace TrfrtSbmt.Api.Features.Voting
 {
     public class GetVoteTally
     {
-        public record GetVoteTallyQuery(string FestivalId, string FortId) : IRequest<List<VoteTallyResult>>;
-        public record VoteTallyResult(string Id, string Name, string State, string City, string Country, string Image, string FortId, string AverageScore, string NumberOfVotes);
-        public class GetVoteTallyQueryHandler : IRequestHandler<GetVoteTallyQuery, List<VoteTallyResult>>
+        public enum Sort
+        {
+            HighToLow,
+            LowToHigh
+        }
+        public record GetVoteTallyQuery(string FestivalId, string FortId, Sort Sort = Sort.HighToLow, int PageSize = 20, string? PaginationKey = null) : IRequest<VoteTallyResult> 
+        {
+            internal Dictionary<string, AttributeValue>? ExclusiveStartKey => GetLastEvaluatedKey(PaginationKey);
+            private Dictionary<string, AttributeValue>? GetLastEvaluatedKey(string? paginationKey)
+            {
+                if (string.IsNullOrEmpty(paginationKey)) return null;
+                paginationKey = Encoding.UTF8.GetString(Convert.FromBase64String(paginationKey));
+                return new Dictionary<string, AttributeValue>
+                {
+                    [nameof(SubmissionRank.PartitionKey)] = new AttributeValue { S = paginationKey?.Split('|')[0] },
+                    [nameof(SubmissionRank.SortKey)] = new AttributeValue { S = paginationKey?.Split('|')[1] },
+                    [nameof(SubmissionRank.FortId)] = new AttributeValue { S = paginationKey?.Split('|')[2] },
+                    [nameof(SubmissionRank.AverageScore)] = new AttributeValue { S = paginationKey?.Split('|')[3] }
+                };
+            }
+        }
+        public record VoteTallyResult(IEnumerable<VoteTallyViewModel> Results, int PageSize = 20, string? PaginationKey = null)
+        {
+            public VoteTallyResult(IEnumerable<SubmissionRank> results, int pageSize, Dictionary<string, AttributeValue>? lastEvaluatedKey) :
+            this(results.Select(r => new VoteTallyViewModel(r)), pageSize, GetPaginationKey(lastEvaluatedKey))
+            { }
+            private static string? GetPaginationKey(Dictionary<string, AttributeValue>? lastEvaluatedKey)
+            {
+                if (lastEvaluatedKey == null) return null;
+                if (!lastEvaluatedKey.ContainsKey(nameof(BaseEntity.PartitionKey))) return null;
+                if (!lastEvaluatedKey.ContainsKey(nameof(BaseEntity.SortKey))) return null;
+                if (!lastEvaluatedKey.ContainsKey(nameof(SubmissionRank.FortId))) return null;
+                if (!lastEvaluatedKey.ContainsKey(nameof(SubmissionRank.AverageScore))) return null;
+                var paginationKey = $"{lastEvaluatedKey[nameof(BaseEntity.PartitionKey)].S}|{lastEvaluatedKey[nameof(BaseEntity.SortKey)].S}|{lastEvaluatedKey[nameof(Submission.FortId)].S}|{lastEvaluatedKey[nameof(SubmissionRank.AverageScore)].S}";
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(paginationKey));
+            }
+        }
+        public record VoteTallyViewModel(string Id, string Name, string State, string City, string Country, string Image, string FortId, string AverageScore, string NumberOfVotes) 
+        {
+            public VoteTallyViewModel(SubmissionRank submissionRank) : this(submissionRank.SubmissionEntityId, submissionRank.Name, submissionRank.State, submissionRank.City, submissionRank.Country, submissionRank.Image, submissionRank.FortId, submissionRank.AverageScore, submissionRank.NumberOfVotes)
+            { }
+        }
+        public class GetVoteTallyQueryHandler : IRequestHandler<GetVoteTallyQuery, VoteTallyResult>
         {
             private readonly IAmazonDynamoDB _db;
             private readonly AppSettings _settings;
@@ -18,18 +60,11 @@ namespace TrfrtSbmt.Api.Features.Voting
                 _settings = settings;
             }
             
-            public async Task<List<VoteTallyResult>> Handle(GetVoteTallyQuery request, CancellationToken cancellationToken)
+            public async Task<VoteTallyResult> Handle(GetVoteTallyQuery request, CancellationToken cancellationToken)
             {
-                var queryResult = await new DynamoDbQueries.VoteTallyQuery(_db, _settings).ExecuteAsync(request.FortId, new Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue>());
+                var queryResult = await new DynamoDbQueries.VoteTallyQuery(_db, _settings).ExecuteAsync(request.FortId, request.Sort == Sort.LowToHigh, request.PageSize, request.ExclusiveStartKey);
                 var ranks = queryResult.Items.Select(i => new SubmissionRank(i)).ToList();
-
-                if (queryResult.LastEvaluatedKey.Any())
-                {
-                    queryResult = await new DynamoDbQueries.VoteTallyQuery(_db, _settings).ExecuteAsync(request.FortId, queryResult.LastEvaluatedKey);
-                    ranks.AddRange(queryResult.Items.Select(i => new SubmissionRank(i)));
-                }
-
-                return ranks.Select(r => new VoteTallyResult(r.SubmissionEntityId, r.Name, r.State, r.City, r.Country, r.Image, r.FortId, r.Rank, r.NumberOfVotes)).ToList();
+                return new VoteTallyResult(ranks, request.PageSize, queryResult.LastEvaluatedKey);
             }
         }
     }
